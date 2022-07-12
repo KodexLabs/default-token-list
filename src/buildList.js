641cb7b73c9bf81ed7b82fa0b4e50dd4140f646c
p7m <perpetuum7@protonmail.com>
2022-07-12 20:29:14 +0200

    fix: invalid casing for tag

--- src/buildList.js
+++ src/buildList.js
@@ -3,33 +3,45 @@ const aurora = require("./tokens/aurora.json");
 const boba = require("./tokens/boba.json");
 
 module.exports = function buildList() {
-  const parsed = version.split(".");
-  return {
-    name: "Koyo Finance list",
-    timestamp: new Date().toISOString(),
-    version: {
-      major: +parsed[0],
-      minor: +parsed[1],
-      patch: +parsed[2],
-    },
-    tags: {
-      stablecoin: {
-        name: "Stablecoin",
-        description:
-          "Tokens that are fixed to an external asset, e.g. the US dollar",
-      },
-      liquidStake: {
-          name: "Liquid staking token",
-          description: "Tokens that represent a liquid alternative to a staked asset, e.g. ETH and stETH or NEAR and stNEAR"
-      }
-    },
-    logoURI: "ipfs://QmXRLVmTmtpPNFfCaWDpkjE4tGFt5GxVJkmekRP3d9HGYm",
-    keywords: ["koyo", "stableswap", "weighted", "dex", "amm", "boba", "aurora", "default"],
-    tokens: [...aurora, ...boba].sort((t1, t2) => {
-      if (t1.chainId === t2.chainId) {
-        return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
-      }
-      return t1.chainId < t2.chainId ? -1 : 1;
-    }),
-  };
+    const parsed = version.split(".");
+    return {
+        name: "Koyo Finance list",
+        timestamp: new Date().toISOString(),
+        version: {
+            major: +parsed[0],
+            minor: +parsed[1],
+            patch: +parsed[2],
+        },
+        tags: {
+            stablecoin: {
+                name: "Stablecoin",
+                description:
+                    "Tokens that are fixed to an external asset, e.g. the US dollar",
+            },
+            "liquid-stake": {
+                name: "Liquid staking token",
+                description:
+                    "Tokens that represent a liquid alternative to a staked asset, e.g. ETH and stETH or NEAR and stNEAR",
+            },
+        },
+        logoURI: "ipfs://QmXRLVmTmtpPNFfCaWDpkjE4tGFt5GxVJkmekRP3d9HGYm",
+        keywords: [
+            "koyo",
+            "stableswap",
+            "weighted",
+            "dex",
+            "amm",
+            "boba",
+            "aurora",
+            "default",
+        ],
+        tokens: [...aurora, ...boba].sort((t1, t2) => {
+            if (t1.chainId === t2.chainId) {
+                return t1.symbol.toLowerCase() < t2.symbol.toLowerCase()
+                    ? -1
+                    : 1;
+            }
+            return t1.chainId < t2.chainId ? -1 : 1;
+        }),
+    };
 };

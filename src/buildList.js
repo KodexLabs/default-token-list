36bcffe7576aa0020c90dfa2a56870a40c1eb9ad
p7m <perpetuum7@protonmail.com>
2023-12-11 17:42:06 +0100

    adjust list

--- src/buildList.js
+++ src/buildList.js
@@ -1,13 +1,10 @@
 const { version } = require("../package.json");
-const aurora = require("./tokens/aurora.json");
-const boba = require("./tokens/boba.json");
-const moonriver = require("./tokens/moonriver.json");
-const polygon = require("./tokens/polygon.json");
+const ethereum = require("./tokens/ethereum.json");
 
 module.exports = function buildList() {
     const parsed = version.split(".");
     return {
-        name: "Koyo Finance list",
+        name: "Kodex list",
         timestamp: new Date().toISOString(),
         version: {
             major: +parsed[0],
@@ -20,52 +17,17 @@ module.exports = function buildList() {
                 description:
                     "Tokens that are fixed to an external asset, e.g. the US dollar",
             },
-            lstake: {
-                name: "Liq Staking Tokens",
-                description:
-                    "Tokens that represent a liquid alternative to a staked asset, e.g. ETH and stETH or NEAR and stNEAR",
-            },
-            xc: {
-                name: "XChain Tokens",
-                description:
-                    "Tokens that are available on all Pokadot and Kusama parachains",
-            },
-            multichain: {
-                name: "Multichain Tokens",
-                description:
-                    "Tokens that where bridged over from other networks using the Multichain bridge",
-            },
-            gov: {
-                name: "Governance Tokens",
-                description:
-                    "Tokens whose holders have voting rights to the corresponding protocol",
-            },
-            dex: {
-                name: "DEX Tokens",
-                description: "Protocol tokens of Decentralized Exchanges",
-            },
-            bodh: {
-                name: "Bodh bTokens",
-                description:
-                    "Tokens that generate interest on the Bodh Finance Protocol",
-            },
         },
-        logoURI: "https://tassets.koyo.finance/token-lists/koyo.png",
+        logoURI: "https://tokens.kodex.io/token-lists/kodex.png",
         keywords: [
-            "koyo",
-            "stableswap",
-            "weighted",
-            "dex",
-            "amm",
-            "boba",
-            "aurora",
-            "polygon",
-            "matic",
-            "moonriver",
-            "moonbeam",
+            "kodex",
             "default",
+            "token",
+            "list",
+            "mainnet",
+            "ethereum"
         ],
-        tokens: [...aurora, ...boba, ...moonriver, ...polygon].sort(
+        tokens: [...ethereum].sort(
             (t1, t2) => {
                 if (t1.chainId === t2.chainId) {
                     return t1.symbol.toLowerCase() < t2.symbol.toLowerCase()
